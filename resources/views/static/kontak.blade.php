422d36782cef7462dac4d35fd9cc454078ef1f8c
Ade Prast <adecma18@gmail.com>
2011-01-01 00:29:42 +0800

    fix kontak

--- resources/views/static/kontak.blade.php
+++ resources/views/static/kontak.blade.php
@@ -8,8 +8,14 @@
 					Kontak
 				</div>
 				<div class="panel-body">
-					<p class="text-center">
-						Deskripsi kontak
+					<h3>Intraco Penta Prima Service (IPPS)</h2>
+					<p class="text-center">				
+						<i class="fa fa-map-marker"></i> Jl. Jendral Ahmad Yani Km 6 no 9, Komp. Kencana, Kec. Banjarmasin Timur, Kota Banjarmasin, Kalimantan Selatan 70249
+						<br>
+						<i class="fa fa-phone"></i> 0511 - 3252634
+						<br>
+						<i class="fa fa-link"></i> http://www.intracopenta.com
+
 					</p>
 				</div>
 			</div>
