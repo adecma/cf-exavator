946edd3fc61c0be679c78a7ab59ce9fda85e31c0
Ade Prast <adecma18@gmail.com>
2016-12-30 05:17:08 +0800

    Add text on home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,15 @@
                     
                     <hr>
                     <p class="text-center">
-                        <img src="/img/kantor-crop.jpg" style="height:320px; margin:0 auto; display: inline;" alt="PT. Intraco Penta Prima Service" class="img-responsive">
+                        <img src="/img/kantor-crop.jpg" style="height:250px; margin:0 auto; display: inline;" alt="PT. Intraco Penta Prima Service" class="img-responsive">
+                    </p>
+                    <p class="text-justify">
+                        Apa itu Excavator <br>
+                        Excavator atau ekskavator adalah alat berat yang yang terdiri dari beberapa bagian yang masing-masing memiliki fungsi tersendiri. Alat berat yang biasanya didominasi warna kuning tersebut terdiri dari bahu (boom), lengan (arm), keranjang atau alat keruk (bucket), kabin dan tracker. Kabin berada di atas tracker yang hadir dilengkap dengan roda rantai.
+                        <br><br>
+                        Pada umumnya alat berat Excavator digerakkan dengan tenaga hidrolis mesin diesel dan berjalan di atas kaki roda rantai. Bisa dibilang Excavator termasuk alat berat yang multifungsi lantaran dapat dimanfaatkan untuk berbagai macam pekerjaan berat. Karenanya tidak heran bila alat ekskavator juga bisa digunakan untuk pekerjaan berat di darat tetapi juga di atas air.
+                         <br><br>
+                        Karena excavator termasuk alat multi fungsi sehingga sering terjadi kerusakan yang akhirnya dibuat website Pendiagnosa Kerusakan Excavator ini.
                     </p>
                     
                 </div>
